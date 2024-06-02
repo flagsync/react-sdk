95512e4da9c7220df1459f8f0d8747f5d7d71f65
Mike Chabot <mike.chabot@live.com>
2024-06-01 23:07:10 -0400

    Export useTrack

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { FlagSyncFlag } from './FlagSyncFlag';
 export { FlagSyncFlags } from './FlagSyncFlags';
 export { useFlag } from './use-flag';
 export { useFlags } from './use-flags';
+export { useTrack } from './use-track';
 export { useFlagSyncClient } from './use-flagsync-client';
 export { useFlagSyncProviderContext } from './flagsync-provider';
 export { FlagSyncProvider } from './flagsync-provider';
