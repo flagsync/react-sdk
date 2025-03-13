024c22ba72559db2a3dcd5d0ecc28bec4b025c13
Mike Chabot <mike.chabot@live.com>
2025-03-13 00:52:52 -0400

    Invert replaceable metadata name/version

--- src/utils.ts
+++ src/utils.ts
@@ -7,8 +7,8 @@ export function getFlagSyncClient(config: FsConfig): DecoratedFsClient {
   const factory = FlagSyncFactory({
     ...config,
     metadata: {
-      sdkName: '__SDK_VERSION__',
-      sdkVersion: '__SDK_NAME__',
+      sdkName: '__SDK_NAME__',
+      sdkVersion: '__SDK_VERSION__',
     },
   });
   const client = factory.client() as DecoratedFsClient;
