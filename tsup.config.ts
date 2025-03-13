559874c906206acd806ed060260d2c5e1e9f79d3
Mike Chabot <mike.chabot@live.com>
2025-03-13 01:55:15 -0400

    Fix anticipated version in build assets

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,14 @@
 import replaceInFile from 'replace-in-file';
+import semver from 'semver';
 import { Options, defineConfig } from 'tsup';
 
 import packageJson from './package.json';
 
 async function injectSdkDetails(dist: string) {
+  const anticipatedVersion = getAnticipatedVersion();
+
+  console.log(`[build] anticipated version tag: ${anticipatedVersion}`);
+
   try {
     await replaceInFile({
       files: `${dist}/**/*`,
@@ -18,6 +23,28 @@ async function injectSdkDetails(dist: string) {
   } catch (error) {
     console.error('[injectSdkDetails]:', error);
   }
+}
+
+function getAnticipatedVersion(): string {
+  const current = packageJson.version;
+
+  console.log(`[build] current version tag: ${current}`);
+
+  if (process.env.MINOR === 'true') {
+    console.log(`[build] bumping minor`);
+    return semver.inc(current, 'minor')!;
+  }
+  if (process.env.PATCH === 'true') {
+    console.log(`[build] bumping patch`);
+    return semver.inc(current, 'patch')!;
+  }
+  if (process.env.PRERELEASE === 'true') {
+    console.log(`[build] bumping prerelease`);
+    return semver.inc(current, 'prerelease', 'alpha')!;
+  }
+
+  console.log(`[build] no env release type found, skipping bump`);
+  return current;
 }
 
 export function modernConfig(opts: Options): Options {
