764b0cca81320f9177dec20a18e30a57407d32a3
Mike Chabot <mike.chabot@live.com>
2025-06-11 12:57:37 -0400

    Ensure availabilty on sdk init error

--- src/utils.ts
+++ src/utils.ts
@@ -1,51 +1,57 @@
 import { FlagSyncFactory, FsErrorEvent, FsEvent } from '@flagsync/js-sdk';
 import type { FsConfig } from '@flagsync/js-sdk';
 
+import { createMockClient } from '~sdk/create-mock-client';
 import { DecoratedFsClient } from '~sdk/types';
 
 export function getFlagSyncClient(config: FsConfig): DecoratedFsClient {
-  const factory = FlagSyncFactory({
-    ...config,
-    metadata: {
-      sdkName: '__SDK_NAME__',
-      sdkVersion: '__SDK_VERSION__',
-    },
-  });
-  const client = factory.client() as DecoratedFsClient;
+  try {
+    const factory = FlagSyncFactory({
+      ...config,
+      metadata: {
+        sdkName: '__SDK_NAME__',
+        sdkVersion: '__SDK_VERSION__',
+      },
+    });
 
-  client.lastUpdated = 0;
-  client.isReady = false;
-  client.isReadyFromStore = false;
-  client.isError = false;
+    const client = factory.client() as DecoratedFsClient;
 
-  function setLastUpdated() {
-    const now = Date.now();
-    if (now > client.lastUpdated) {
-      client.lastUpdated = now;
+    client.lastUpdated = 0;
+    client.isReady = false;
+    client.isReadyFromStore = false;
+    client.isError = false;
+
+    function setLastUpdated() {
+      const now = Date.now();
+      if (now > client.lastUpdated) {
+        client.lastUpdated = now;
+      }
     }
+
+    function onReady() {
+      client.isReady = true;
+      client.isReadyFromStore = true;
+      setLastUpdated();
+    }
+
+    function onReadyFromStore() {
+      client.isReadyFromStore = true;
+      setLastUpdated();
+    }
+
+    function onError(error: FsErrorEvent) {
+      client.isError = true;
+      console.error(`FlagSync: SDK initialization error (type: ${error.type})`);
+      console.error(error.error);
+    }
+
+    client.on(FsEvent.SDK_UPDATE, setLastUpdated);
+    client.on(FsEvent.SDK_READY, onReady);
+    client.on(FsEvent.SDK_READY_FROM_STORE, onReadyFromStore);
+    client.on(FsEvent.ERROR, onError);
+
+    return client;
+  } catch (e) {
+    return createMockClient(e);
   }
-
-  function onReady() {
-    client.isReady = true;
-    client.isReadyFromStore = true;
-    setLastUpdated();
-  }
-
-  function onReadyFromStore() {
-    client.isReadyFromStore = true;
-    setLastUpdated();
-  }
-
-  function onError(error: FsErrorEvent) {
-    client.isError = true;
-    console.error(`FlagSync: SDK initialization error (type: ${error.type})`);
-    console.error(error.error);
-  }
-
-  client.on(FsEvent.SDK_UPDATE, setLastUpdated);
-  client.on(FsEvent.SDK_READY, onReady);
-  client.on(FsEvent.SDK_READY_FROM_STORE, onReadyFromStore);
-  client.on(FsEvent.ERROR, onError);
-
-  return client;
 }
